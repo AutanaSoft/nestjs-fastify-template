3e31d1d12654637ccad1c29eac3ce37bccf08fb9
AutanaSoft <admin@autanasoft.com>
2025-07-30 20:37:04 +0000

    feat(user): setup prisma and initial user migration
    - Initializes Prisma for database management and migrations.
    - Defines the initial schema for the User model, including roles and status.
    - Adds the first database migration to create the users table.
    - Implements a seeding script to create a default admin user.
    - Updates docker-compose to

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const email = 'admin@admin.com';
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash('admin', salt);
+
+  await prisma.user.upsert({
+    where: { email },
+    update: {},
+    create: {
+      email,
+      password: hashedPassword,
+      role: 'ADMIN',
+    },
+  });
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
